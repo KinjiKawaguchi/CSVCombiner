cd485565c28e5e97a749d4500a6882e4846a40da
KinjiKawaguchi <kawakin0310@icloud.com>
2022-10-21 19:59:58 +0900

    ドラッグアンドドロップフレーム作成

--- CSVCombiner/MainWindow.xaml.cs
+++ CSVCombiner/MainWindow.xaml.cs
@@ -20,9 +20,57 @@ namespace CSVCombiner
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static class Global////Global変数定義
+        {
+            public static string File1_Path = "";
+            public static string File2_Path = "";
+            public static bool File1_Exist = false;
+            public static bool File2_Exist = false;
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+
+            EnableDragDrop(Frame_DropFile1);
+            EnableDragDrop(Frame_DropFile2);
+        }
+
+        private void EnableDragDrop(Control control)
+        {
+            //ドラッグ＆ドロップを受け付けられるようにする
+            control.AllowDrop = true;
+
+            //ドラッグが開始された時のイベント処理（マウスカーソルをドラッグ中のアイコンに変更）
+            control.PreviewDragOver += (s, e) =>
+            {
+                //ファイルがドラッグされたとき、カーソルをドラッグ中のアイコンに変更し、そうでない場合は何もしない。
+                e.Effects = (e.Data.GetDataPresent(DataFormats.FileDrop)) ? DragDropEffects.Copy : e.Effects = DragDropEffects.None;
+                e.Handled = true;
+            };
+
+            //ドラッグ＆ドロップが完了した時の処理（ファイル名を取得し、ファイルの中身をTextプロパティに代入）
+            control.PreviewDrop += (s, e) =>
+            {
+                if (e.Data.GetDataPresent(DataFormats.FileDrop)) // ドロップされたものがファイルかどうか確認する。
+                {
+                    string[] paths = ((string[])e.Data.GetData(DataFormats.FileDrop));
+                    if (control.Name == "Frame_DropFile1")
+                    {
+                        Global.File1_Path = paths[0];
+                        Global.File1_Exist = true;
+                    }
+                    else
+                    {
+                        Global.File2_Path = paths[0];
+                        Global.File2_Exist = true;
+                    }
+                    if(Global.File1_Exist && Global.File2_Exist)
+                    {
+                        button_execute.visibility == visible;
+                    }
+                }
+            };
         }
     }
 }
